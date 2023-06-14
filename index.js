1a703643b87ba19acd4526496ff248798f159a16
WinterJackson <winterjacksonwj@gmail.com>
2023-06-14 22:36:44 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(spy){
+    spy()
+    }
+    function returnsANamedFunction() {
+        return function namedFunction() {
+          // function body
+        };
+      }
+    
+      function returnsAnAnonymousFunction() {
+        return function() {
+          // function body
+        };
+      }
